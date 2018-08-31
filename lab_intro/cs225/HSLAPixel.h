e0d4853dd2b251356de2bcf9c1125cbcc2be34f4
Morgan Meliment <morganm4@siebl-0224-14.ews.illinois.edu>
2018-08-31 18:18:39 -0500

    Finished first header file.

--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -12,6 +12,17 @@
 #include <sstream>
 
 namespace cs225 {
-}
+  class HSLAPixel {
+    public:
+      double h;
+      double s;
+      double l;
+      double a;
+
+      HSLAPixel();
+      HSLAPixel(double hue, double saturation, double luminance);
+      HSLAPixel(double hue, double saturation, double luminance, double alpha);
+  }
+};
 
 #endif
