f6055aa5d6d3dc0e09f632b912c7d7f86d82615f
morganmeliment <morganmeliment@gmail.com>
2018-09-14 18:12:37 -0500

    fix1

--- lab_memory/allocator.cpp
+++ lab_memory/allocator.cpp
@@ -47,12 +47,13 @@ void Allocator::loadRooms(const string& file)
 {
     // Read in rooms
     fileio::loadRooms(file);
+    roomCount = fileio::getNumRooms();
     rooms = new Room[roomCount];
 
     totalCapacity = 0;
     int i = 0;
     while (fileio::areMoreRooms()) {
-        i++; 
+        i++;
         rooms[i] = fileio::nextRoom();
         totalCapacity += rooms[i].capacity;
     }
