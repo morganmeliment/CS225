b02a55d39174da442064acb593e82707c462a6c7
morganmeliment <morganmeliment@gmail.com>
2018-09-21 18:19:57 -0500

    Finished lab excercise 1

--- lab_inheritance/shape.h
+++ lab_inheritance/shape.h
@@ -47,14 +47,14 @@ class Shape : public Drawable
      *
      * @return the area (in pixels) of the Shape
      */
-    int area() const;
+    virtual int area() const;
 
     /**
      * @brief Computes and returns the perimeter of the Shape in pixels
      *
      * @return the perimeter (in pixels) of the Shape
      */
-    int perimeter() const;
+    virtual int perimeter() const;
 
     /**
      * @brief Checks to see if the Vector2 p is contained inside of the Shape
@@ -88,4 +88,3 @@ class Shape : public Drawable
 
     void draw(PNG* canvas) const;
 };
-
